95d037b0655dae786f73b441b9681963fd449737
Kris <kris.shanahan@syntystudios.com>
2024-05-16 08:14:32 +0930

    Adjustments to command handling

--- src/main/java/io/shantek/functions/Commands.java
+++ src/main/java/io/shantek/functions/Commands.java
@@ -18,30 +18,43 @@ public class Commands implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("postoffice")) {
-            if (args.length == 2 && args[0].equalsIgnoreCase("barrelname")) {
-                // Check if the second argument is present
-                String name = args[1];
-                postOffice.customBarrelName = name;
+            if (args.length > 1 && args[0].equalsIgnoreCase("barrelname")) {
 
-                postOffice.pluginConfig.setCustomBarrelName(postOffice.customBarrelName);
-                sender.sendMessage("Custom barrel name set to " + postOffice.customBarrelName);
-                return true;
+                if (args.length > 2) {
+
+                    // They are sending something longer than they should be
+                    sender.sendMessage(ChatColor.RED + "Invalid barrel name, use /postoffice barrelname <name>");
+                    return true;
+
+                } else {
+
+                    // The length seems valid, time to process it
+                    String name = args[1];
+                    postOffice.customBarrelName = name;
+
+                    postOffice.pluginConfig.setCustomBarrelName(postOffice.customBarrelName);
+                    sender.sendMessage("Custom barrel name set to " + postOffice.customBarrelName);
+                    return true;
+                }
             } else if (args.length == 1 && args[0].equalsIgnoreCase("barrelname")) {
+
+                // They sent /postoffice barrelname without actually defining a name to use
                 sender.sendMessage(ChatColor.RED + "Invalid barrel name, use /postoffice barrelname <name>");
                 return true;
 
             } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
                 if (sender.hasPermission("shantek.postoffice.reload") || sender.isOp()) {
-                    // Reload logic
+
+                    // Reload all the config and language file
                     postOffice.pluginConfig.reloadConfigFile();
-                    sender.sendMessage(ChatColor.GREEN + "PostOffice config reloaded.");
+                    sender.sendMessage(ChatColor.GREEN + "Post Office config file has been reloaded.");
                 } else {
                     sender.sendMessage(ChatColor.RED + "You don't have access to this command!");
                 }
                 return true;
             } else {
                 // Invalid command format
-                sender.sendMessage(ChatColor.RED + "Invalid command");
+                sender.sendMessage(ChatColor.RED + "Unknown command or insufficient permission.");
                 return false;
             }
         } else {
