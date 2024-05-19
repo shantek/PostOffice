eeb5f14f7f71f3c8c68710e743d91345cbdb5969
Kris <kris.shanahan@syntystudios.com>
2024-05-19 12:32:09 +0930

    Fixed issues with player join listener not firing

--- src/main/java/io/shantek/listeners/PlayerJoin.java
+++ src/main/java/io/shantek/listeners/PlayerJoin.java
@@ -8,30 +8,28 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-
 public class PlayerJoin implements Listener {
 
     public PostOffice postOffice;
+
     public PlayerJoin(PostOffice postOffice) {
         this.postOffice = postOffice;
     }
 
-    private class PlayerLoginListener implements Listener {
-        @EventHandler
-        public void onPlayerLogin(PlayerJoinEvent event) {
-            Player player = event.getPlayer();
+    @EventHandler
+    public void onPlayerLogin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
 
-            if (postOffice.playersWithMail.contains(player.getUniqueId())) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', postOffice.language.gotMailMessage));
-            }
-
-            if (postOffice.updateNotificationEnabled && (player.isOp() || player.hasPermission("shantek.postoffice.updatenotification"))) {
-                if (UpdateChecker.isNewVersionAvailable(postOffice.getDescription().getVersion(), UpdateChecker.remoteVersion)) {
-                    player.sendMessage("[Post Office] An update is available! New version: " + UpdateChecker.remoteVersion);
-                }
-            }
-
+        if (postOffice.playersWithMail.contains(player.getName())) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', postOffice.language.gotMailMessage));
         }
-    }
 
+        if (postOffice.updateNotificationEnabled && (player.isOp() || player.hasPermission("shantek.postoffice.updatenotification"))) {
+            if (UpdateChecker.isNewVersionAvailable(postOffice.getDescription().getVersion(), UpdateChecker.remoteVersion)) {
+                player.sendMessage("[Post Office] An update is available! New version: " + UpdateChecker.remoteVersion);
+            }
+        }
+
+    }
 }
+
