49b1f4ad66789e2b2243bf6121e4fdc6a91db8fe
Kris <kris@shantek.com.au>
2024-08-29 08:19:12 +0930

    Added extra checks to post box protection

--- src/main/java/io/shantek/listeners/BarrelProtection.java
+++ src/main/java/io/shantek/listeners/BarrelProtection.java
@@ -4,7 +4,9 @@ import io.shantek.PostOffice;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.*;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.WitherSkull;
 import org.bukkit.entity.minecart.HopperMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,7 +14,9 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.inventory.InventoryHolder;
 
@@ -34,26 +38,16 @@ public class BarrelProtection implements Listener {
         Block signBlock = event.getBlock();
 
         if (hasBarrelNearby(signBlock)) {
-
             if (!player.isOp() && !player.hasPermission("shantek.postoffice.create")) {
-
-                if (postOffice.postBoxProtection) {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', postOffice.language.createError));
-                    event.setCancelled(true);
-                }
-
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', postOffice.language.createError));
+                event.setCancelled(true);
             } else {
-                // Check if line 2 of the sign has text
                 String line2 = event.getLine(1);
                 if (line2 != null && !line2.isEmpty()) {
-                    // Get the player name
-
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                             postOffice.language.postboxCreated
                                     .replace("%username%", line2)));
-
                 }
-
             }
         }
     }
@@ -72,31 +66,22 @@ public class BarrelProtection implements Listener {
             Barrel barrel = (Barrel) brokenBlock.getState();
             String barrelCustomName = barrel.getCustomName();
 
-            // Check if it's a custom post box and the player has permission
             if (barrelCustomName != null && barrelCustomName.equalsIgnoreCase(postOffice.customBarrelName)) {
-
                 if (!player.isOp() && !player.hasPermission("shantek.postoffice.break")) {
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', postOffice.language.breakError));
                     event.setCancelled(true);
                 }
-
             }
-
         }
 
         // Check if the broken block is a sign
         if (brokenBlock.getState() instanceof Sign) {
-
-            if (hasBarrelNearby(brokenBlock))
-
-
+            if (hasBarrelNearby(brokenBlock)) {
                 if (!player.isOp() && !player.hasPermission("shantek.postoffice.break")) {
-
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', postOffice.language.breakError));
                     event.setCancelled(true);
                 }
-
-
+            }
         }
     }
 
@@ -109,20 +94,12 @@ public class BarrelProtection implements Listener {
         Player player = event.getPlayer();
         Block placedBlock = event.getBlockPlaced();
 
-        if (!(placedBlock.getState() instanceof Sign)) {
-            return; // Ignore blocks that are not signs
-        }
-
-        if (hasBarrelNearby(placedBlock)) {
+        if (placedBlock.getState() instanceof Sign && hasBarrelNearby(placedBlock)) {
             if (!player.isOp() && !player.hasPermission("shantek.postoffice.create")) {
-                // Player doesn't have permission to place a sign on the barrel
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', postOffice.language.createError));
-
                 placedBlock.breakNaturally();
-                // event.setCancelled(true);
             }
         }
-
     }
 
     private boolean hasBarrelNearby(Block block) {
@@ -165,15 +142,12 @@ public class BarrelProtection implements Listener {
         // Check if the source is a barrel and the destination is a hopper or hopper minecart
         if (sourceHolder instanceof Barrel && (destinationHolder instanceof Hopper || destinationHolder instanceof HopperMinecart)) {
             Barrel barrel = (Barrel) sourceHolder;
-
-            // Check if the barrel has the custom name
             String barrelCustomName = barrel.getCustomName();
             if (barrelCustomName != null && barrelCustomName.equalsIgnoreCase(postOffice.customBarrelName)) {
                 event.setCancelled(true);
             }
         }
     }
-
 
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent event) {
@@ -189,5 +163,34 @@ public class BarrelProtection implements Listener {
             return;
         }
         event.blockList().removeIf(postOffice.helpers::isProtectedPostBox);
+    }
+
+    @EventHandler
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        if (!postOffice.postBoxProtection) {
+            return;
+        }
+
+        if (event.getEntityType() == EntityType.WITHER) {
+            Block block = event.getBlock();
+            if (postOffice.helpers.isProtectedPostBox(block)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event) {
+        if (!postOffice.postBoxProtection) {
+            return;
+        }
+
+        if (event.getEntity() instanceof WitherSkull) {
+            Block hitBlock = event.getHitBlock();
+            if (hitBlock != null && postOffice.helpers.isProtectedPostBox(hitBlock)) {
+                // Cancel explosion effect by removing the wither skull
+                event.getEntity().remove();
+            }
+        }
     }
 }
