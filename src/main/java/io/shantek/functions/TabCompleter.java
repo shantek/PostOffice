e9838b3291ef88e648cb2db233bda591fbeca6b9
Kris <kris@shantek.com.au>
2024-09-18 09:27:58 +0930

    Added new commands to tab completer

--- src/main/java/io/shantek/functions/TabCompleter.java
+++ src/main/java/io/shantek/functions/TabCompleter.java
@@ -1,14 +1,20 @@
 package io.shantek.functions;
 
 import io.shantek.PostOffice;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TabCompleter implements org.bukkit.command.TabCompleter {
 
     public PostOffice postOffice;
+
     public TabCompleter(PostOffice postOffice) {
         this.postOffice = postOffice;
     }
@@ -19,13 +25,40 @@ public class TabCompleter implements org.bukkit.command.TabCompleter {
 
         if (cmd.getName().equalsIgnoreCase("postoffice")) {
             if (args.length == 1) {
-                // Check the first argument
-                if ("reload".startsWith(args[0].toLowerCase()) && sender.hasPermission("shantek.postoffice.reload")) {
+                // Add commands based on permissions
+                if (sender.hasPermission("shantek.postoffice.reload")) {
                     completions.add("reload");
                 }
+                if (sender.hasPermission("shantek.postoffice.claim") || sender.hasPermission("shantek.postoffice.claim.others")) {
+                    completions.add("claim");
+                }
+                if (sender.hasPermission("shantek.postoffice.register")) {
+                    completions.add("register");
+                    completions.add("remove");
+                }
+
+                // Commands all players have access to
+                completions.add("info");
+
+                // Filter completions based on partial input
+                return completions.stream()
+                        .filter(c -> c.startsWith(args[0].toLowerCase()))
+                        .sorted()
+                        .collect(Collectors.toList());
+            } else if (args.length == 2 && args[0].equalsIgnoreCase("claim") && sender.hasPermission("shantek.postoffice.claim.others")) {
+                // Populate online players' names for claim.others permission
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    completions.add(player.getName());
+                }
+
+                // Filter players based on partial input
+                return completions.stream()
+                        .filter(c -> c.toLowerCase().startsWith(args[1].toLowerCase()))
+                        .sorted()
+                        .collect(Collectors.toList());
             }
         }
-        return completions;
-    }
 
+        return Collections.emptyList();
+    }
 }
